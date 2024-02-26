1754de52c0f32f256bd1b4753694babd38f0674e
Paulo Renan Nascimento de Almeida <able.paulorenan@gmail.com>
2024-02-26 17:08:38 -0300

    finish nutrition part

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,16 +10,7 @@ export class ProductsService {
             const page = await browser.newPage();
             page.setDefaultNavigationTimeout(0);
             await page.goto(`https://br.openfoodfacts.org/produto/${product}`);
-            const productData = await page.evaluate(() => {
-                const pageError = document.querySelector('.if-empty-dnone');
-                if (pageError) {
-                    console.log('pageError', pageError.textContent);
-                    return { error: pageError.textContent };
-                }
-                const name = document.querySelector('.title-1').textContent;
-                return { name, teste: 'teste' };
-            });
-            console.log('productData', productData);
+            const productData = await this.filterProductData(page);
             return productData;
         } catch (error) {
             console.log('error', error);
@@ -27,5 +18,77 @@ export class ProductsService {
         } finally {
             await browser.close();
         }
+    }
+
+    async filterProductData(page) {
+        const productData = await page.evaluate(() => {
+            const pageError = document.querySelector('.if-empty-dnone');
+            if (pageError) {
+                console.log('pageError', pageError.textContent);
+                return { error: pageError.textContent };
+            }
+
+            const getNutriValues = () => {
+                const listOfNutriValues = document.querySelector('#panel_nutrient_levels_content').querySelectorAll('ul.panel_accordion');
+                const nutriValues = [];
+    
+                listOfNutriValues.forEach(item => {
+                    const level = item.querySelector('.accordion-navigation').getElementsByTagName('img')[0].getAttribute('src').split('/').pop().split('.')[0];
+                    const description = item.querySelector('.accordion-navigation').getElementsByTagName('h4')[0].textContent.trim()
+                    nutriValues.push([level, description]);
+                });
+
+                return nutriValues;
+            };
+
+            const getNutriData = () => {
+                    const table = document.querySelector('table[aria-label="Dados nutricionais"]');
+                    const data = {};
+                    const lines = table.querySelectorAll('tbody tr');
+
+                    lines.forEach(line => {
+                        const name = line.querySelector('td:first-of-type').textContent.trim();
+                        const per100g = line.querySelector('td:nth-of-type(2)').textContent.trim();
+                        const perServing = line.querySelector('td:nth-of-type(3)').textContent.trim();
+                        data[name] = {
+                            per100g: per100g,
+                            perServing: perServing
+                        };
+                    });
+
+                    return data;
+            }
+
+            const getNutriServings = () => {
+                const table = document.querySelector('table[aria-label="Dados nutricionais"]');
+                const thLength = table.querySelectorAll('thead th').length;
+                
+                if (thLength < 4) {
+                    return ''
+                }
+
+                const serving = table.querySelector('thead th:nth-of-type(3)').textContent.trim().split('(')[1].split(')')[0];
+                return serving;
+            }
+
+            // return getNutriServings();
+
+            return {
+                title: document.querySelector('.title-1').textContent,
+                quantity: document.querySelector('#field_quantity_value').textContent,
+                nutrition: {
+                    score: document.querySelector('#attributes_grid').firstElementChild.querySelector('.attr_title').textContent.split(' ')[1],
+                    values: getNutriValues(),
+                    servingSize: getNutriServings(),
+                    data: getNutriData(),
+                },
+                nova: {
+                    score: document.querySelector('#attributes_grid').getElementsByTagName('li')[1].querySelector('.attr_title').textContent.split(' ')[1],
+                    title: document.querySelector('#attributes_grid').getElementsByTagName('span')[1].textContent,
+                }
+            };
+        });
+        console.log('productData', productData);
+        return productData;
     }
 }
